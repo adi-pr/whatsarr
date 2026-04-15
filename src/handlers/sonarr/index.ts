f035f93cc7e59be3e5267d2eb28c9c225398d639
Aditya (Ruben) Prasad <adityaprasad7123@gmail.com>
2026-04-15 02:58:13 -0400

    feat: add onHealth handler to process health status events and send notifications

--- src/handlers/sonarr/index.ts
+++ src/handlers/sonarr/index.ts
@@ -2,10 +2,13 @@ import onGrab from "./onGrab";
 import onSeriesAdd from "./onSeriesAdd";
 import onSeriesDelete from "./onSeriesDelete";
 import onTest from "./onTest";
+import onHealth from "./onHealth";
 
 export const handlers = {
     "Test": onTest,
     "Grab": onGrab,
     "SeriesAdd": onSeriesAdd,
     "SeriesDelete": onSeriesDelete,
+    "Health": onHealth,
+    "HealthRestored": onHealth,
 };
---
+++ src/handlers/sonarr/onHealth.ts
@@ -0,0 +1,16 @@
+import { client } from "../../whatsapp/client";
+import type { SonarrHealthPayload } from "../../../types/sonar";
+import env from "../../../config/env";
+
+export default async function onHealth(payload: SonarrHealthPayload) {
+
+    await client.sendMessage(
+        env.ALLIDS[0],
+        `Sonarr Health Status: ${payload.message} \n Level: ${payload.level} \n Type: ${payload.type} \n Instance: ${payload.instanceName}`
+    );
+
+    return {
+        success: true,
+        message: "Health event received and processed successfully"
+    };
+}
